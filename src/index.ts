693e4f6239d6c2c56208be8161f696b9992708ca
ZxBing0066 <ZxBing0066@gmail.com>
2020-09-15 10:46:49 +0800

    add scripts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { import as _import, export as _export } from './mod';
+import config from './config';
+
+const mod = {
+    import: _import,
+    export: _export,
+    config
+};
+
+export default mod;
