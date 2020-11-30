8f9dd0c07f10e87a35596ef39d98c4b7621453a7
ZxBing0066 <ZxBing0066@gmail.com>
2020-11-30 18:05:43 +0800

    fix export bug

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import { import as _import, export as _export, throw as _throw } from './src/mod';
-import config from './src/config';
-
-const mod = {
-    import: _import,
-    export: _export,
-    throw: _throw,
-    config
-};
+import mod from './src';
 
 window.mod = mod;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { import as _import, export as _export } from './mod';
+import { import as _import, export as _export, throw as _throw } from './mod';
 import config from './config';
 
 const mod = {
     import: _import,
     export: _export,
+    throw: _throw,
     config
 };
 
