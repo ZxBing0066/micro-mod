0270562aef66ff22f6fad0e59ab331fab0e5ce11
ZxBing0066 <ZxBing0066@gmail.com>
2020-11-30 17:59:47 +0800

    export throw

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import { import as _import, export as _export } from './src/mod';
+import { import as _import, export as _export, throw as _throw } from './src/mod';
 import config from './src/config';
 
 const mod = {
     import: _import,
     export: _export,
+    throw: _throw,
     config
 };
 
